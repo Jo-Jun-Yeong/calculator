8488214577cbf2895b8f09fbe027837c50b5ea35
apple <apple@218.38.137.28>
2020-09-16 20:28:29 +0900

    inital commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("덧셈  "+(3 + 4));
-		System.out.println("뺄셈  "+(4 - 3));
-		System.out.println("곱셈  "+(3 * 4));
-		System.out.println("나눗셈  "+(4 / 2));
+		System.out.println("두개의 값을 입력하시오");
+		Scanner scanner = new Scanner(System.in);
+		int frist = scanner.nextInt();
+		System.out.print("첫번째 입력 값:    ");
+		System.out.println(frist);
+		
+		int second = scanner.nextInt();
+		System.out.print("번째 입력 값:    ");
+		System.out.println(second);
+		
+		System.out.println("덧셈  "+(frist + second));
+		System.out.println("뺄셈  "+(frist - second));
+		System.out.println("곱셈  "+(frist * second));
+		System.out.println("나눗셈  "+(frist / second));
 	}
-
 }
