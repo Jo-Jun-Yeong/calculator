e927e6a3ae5076bec6db42e6ac8be212bd8ca932
apple <apple@localhost>
2020-09-19 01:41:33 +0900

    다시 작성한 계산기 코드

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("첫번째 값을 입력하세요");
+//		String symbol = scanner.next();
+		System.out.println("수를 입력하세요");
 		int i = scanner.nextInt();
-		int result = i;
+		
+		int again = i;
+		
 		while(true) {
-			System.out.println("기호를 입력하세요 :     ");
+			System.out.println("기호를 입력하세요");
 			String symbol = scanner.next();
-			if ("=".equals(symbol)) {
-				System.out.print("계산 종료   ");
-				System.out.println(result);
+
+		
+			if("=".equals(symbol)) {
+				System.out.print("계산 종료    =");
+				System.out.println(again);
 				break;
 			}
-			
-			System.out.println("두번째 값을 입력하세요");
+				
+			System.out.println("수를 입력하세요");
 			int j = scanner.nextInt();
-		
+			
 			if("+".equals(symbol)) {
-				result = result + j;
-				System.out.println(result);
+				System.out.println(again+j);
+				again = again +j;
 			}
-			else if("-".equals(symbol)) {
-				result = result - j;
-				System.out.println(result);
+			else if("-".equals(symbol)){
+				System.out.println(again-j);
+				again = again - j;
 			}
 			else if("*".equals(symbol)) {
-				result = result * j;
-				System.out.println(result);
+				System.out.println(again*j);
+				again = again * j;
 			}
 			else if("/".equals(symbol)) {
-				result = result / j;
-				System.out.println(result);
+				System.out.println(again/j);
+				again = again * j;
 			}
 			else {
-				System.out.println("사칙연산에 해당되지 않는 입력입니다.");
+				System.out.println("올바른 수식 입력이 아닙니다");
 			}
-		}		
+		}
 	}
 }
