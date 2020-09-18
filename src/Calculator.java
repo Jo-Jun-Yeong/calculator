3d6e1a46bbf84436c19ab8f93e63801d6948d408
apple <apple@218.38.137.28>
2020-09-18 18:01:41 +0900

    계산기 기본 기능 구현 완

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("두개의 값을 입력하시오");
 		Scanner scanner = new Scanner(System.in);
-		int frist = scanner.nextInt();
-		System.out.print("첫번째 입력 값:    ");
-		System.out.println(frist);
+		System.out.println("첫번째 값을 입력하세요");
+		int i = scanner.nextInt();
+		int result = i;
+		while(true) {
+			System.out.println("기호를 입력하세요 :     ");
+			String symbol = scanner.next();
+			if ("=".equals(symbol)) {
+				System.out.print("계산 종료   ");
+				System.out.println(result);
+				break;
+			}
+			
+			System.out.println("두번째 값을 입력하세요");
+			int j = scanner.nextInt();
 		
-		int second = scanner.nextInt();
-		System.out.print("번째 입력 값:    ");
-		System.out.println(second);
-
-		Scanner symbol = new Scanner(System.in);
-		String Symbol = scanner.next();
-		System.out.println("입력한 기호:    " + Symbol);
-		System.out.println(frist + Symbol + second);
-		if("+".equals(Symbol)) {
-			System.out.println(frist + second);
-		}
-		else if("-".equals(Symbol)) {
-			System.out.println(frist - second);
-		}
-		else if ("*".equals(Symbol)) {
-			System.out.println(frist * second);
-		}
-		else if("/".equals(Symbol)) {
-			System.out.println(frist / second);
-		}
-		else {
-			System.out.println("사칙연산에 해당되지 않는 입력입니다.");
-		}
+			if("+".equals(symbol)) {
+				result = result + j;
+				System.out.println(result);
+			}
+			else if("-".equals(symbol)) {
+				result = result - j;
+				System.out.println(result);
+			}
+			else if("*".equals(symbol)) {
+				result = result * j;
+				System.out.println(result);
+			}
+			else if("/".equals(symbol)) {
+				result = result / j;
+				System.out.println(result);
+			}
+			else {
+				System.out.println("사칙연산에 해당되지 않는 입력입니다.");
+			}
+		}		
 	}
 }
