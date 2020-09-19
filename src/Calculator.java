cee6ccf87b8ff29f7242db216b9977f0fa0cb68a
apple <apple@192.168.0.11>
2020-09-20 05:13:21 +0900

    메소드를 이용한 기본적인 계산기 구현(09/20)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,46 +1,65 @@
 import java.util.Scanner;
 public class Calculator {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-//		String symbol = scanner.next();
-		System.out.println("수를 입력하세요");
-		int i = scanner.nextInt();
+		static int getFirstValue(Scanner scanner) {
+			System.out.println("첫번째 값을 입력하세요");
+			int i = scanner.nextInt();
+			return i;
+		}
 		
-		int again = i;
-		
-		while(true) {
-			System.out.println("기호를 입력하세요");
+		static String getSymbol(Scanner scanner) {
+			System.out.println("기호를 입력하세요 :     ");
 			String symbol = scanner.next();
-
+			//if문의 종료문자가 다음으로 와야함
+			return symbol;
+		}
 		
-			if("=".equals(symbol)) {
-				System.out.print("계산 종료    =");
-				System.out.println(again);
-				break;
-			}
-				
-			System.out.println("수를 입력하세요");
+		static int getSecondValue(Scanner scanner) {
+			System.out.println("두번째 값을 입력하세요");
 			int j = scanner.nextInt();
-			
+			return j;
+		}
+		
+		static int calculate(int i,String symbol, int j) {
+			//사칙연산을 이용해서 결과값을 반환하는 열
+			int result = 0;
 			if("+".equals(symbol)) {
-				System.out.println(again+j);
-				again = again +j;
+				result = i + j;
+				System.out.println(result);
 			}
-			else if("-".equals(symbol)){
-				System.out.println(again-j);
-				again = again - j;
+			else if("-".equals(symbol)) {
+				result = i- j;
+				System.out.println(result);
 			}
 			else if("*".equals(symbol)) {
-				System.out.println(again*j);
-				again = again * j;
+				result = i* j;
+				System.out.println(result);
 			}
 			else if("/".equals(symbol)) {
-				System.out.println(again/j);
-				again = again * j;
+				result = i / j;
+				System.out.println(result);
 			}
 			else {
-				System.out.println("올바른 수식 입력이 아닙니다");
+				System.out.println("사칙연산에 해당되지 않는 입력입니다.");
 			}
+			return result;
 		}
+		
+		static void print(int result) {
+			System.out.println("계산 종료   "   +  result);
+		}
+		
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			int i = getFirstValue(scanner);
+			int result = i;
+			while(true) {
+			String  symbol = getSymbol(scanner);
+			if ("=".equals(symbol)) {
+				print(result);
+				break;
+			}				
+			int j =getSecondValue(scanner);
+			result = calculate(result, symbol, j);
+}			
 	}
 }
